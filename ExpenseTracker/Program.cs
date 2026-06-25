0744348b086556c057001937b5a21f0a4bc64990
Benjamin Andoh <benoo.andoh@gmail.com>
2026-06-25 01:09:15 -0400

    build the initial stage of ui

--- ExpenseTracker/Program.cs
+++ ExpenseTracker/Program.cs
@@ -18,9 +18,82 @@
 //
 //  See section 6 of the assignment brief for a sample run to aim for.
 // =====================================================================
+using ExpenseTracker;
+using System.ComponentModel;
+using static ExpenseTracker.BudgetRules;
 
+string doubleLinePrint = "=====================================================================";
 
+Console.WriteLine(doubleLinePrint);
+Console.WriteLine("MyBudget Expense Tracker");
+Console.WriteLine(doubleLinePrint);
 
+Console.WriteLine($" 1) Add an expense   2) View summary   3) Set monthly budget   4) Exit");
+Console.Write(">");
+
+decimal choice = 0;
+bool intake = false;
+
+while (!intake)
+{
+    if (!decimal.TryParse(Console.ReadLine(), out choice) || choice <= 0 || choice >= 4)
+    {
+        Console.WriteLine("Sorry select from the options on the menu");
+        continue;
+    }
+    intake = true;
+}
+
+string Description = "";
+decimal Amount = 0;
+string Category = "";
+DateTime WhatDate = DateTime.Today;
+string Note = "";
+string Recorded = "";
+
+switch (choice)
+{
+    case 1:
+        // Description 
+        Console.WriteLine("Description: ");
+        Console.ReadLine();
+
+        // Amount 
+        Console.WriteLine("Amount     : ");
+        if (!decimal.TryParse(Console.ReadLine(), out Amount))
+        {
+            Console.WriteLine("Amount must be a number");
+        }
+        ;
+        ValidateAmount(Amount);
+
+        // category
+        Console.WriteLine("Category    : [Food/Transport/Utilities/Entertainment/Other] ");
+
+        // Date
+        Console.WriteLine("Date (blank = today): ");
+
+        // Note
+        Console.WriteLine("Note (optional): ");
+
+        // Recorded
+        Console.WriteLine("Recorded: ");
+
+        // Size Band
+        Console.WriteLine("Size band: ");
+
+        // Budget
+        Console.WriteLine("Budget: ");
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+    default:
+        break;
+}
+
+Console.WriteLine($"this is your chioce {choice}");
 
 
 
